acbb80404c26dd05009e40b0daa7771c33f1d57f
Krzysztof Telka <krzysztof.telka@gmail.com>
2013-03-01 00:19:27 +0100

    Lesson2

--- src/pl/selfimproveit/model/User.java
+++ src/pl/selfimproveit/model/User.java
@@ -1,15 +1,21 @@
 package pl.selfimproveit.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "USER")
-public class User {
+@Table(name = "USER", uniqueConstraints = @UniqueConstraint(columnNames = {"NAME","SURNAME"}))
+		
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@Column(name = "UserID")
@@ -22,6 +28,10 @@ public class User {
 	@Column(name = "SURNAME")
 	private String lastName;
 
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
 	public int getId() {
 		return id;
 	}
